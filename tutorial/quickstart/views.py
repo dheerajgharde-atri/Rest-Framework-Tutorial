39ebdaf24695a27407374b030c653417f2c97513
dheerajgharde-atri <gharde@atrihub.io>
2023-06-12 16:13:20 -0700

    feat(project): Django Rest Frameowrk Tutorial
    Rearranged the files in proper order
    
    [ICTRAINING-1522]

---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+
+# Create your views here.
